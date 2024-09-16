6cc2e6bdd28fddfd615f5f1010a709f5dee110a4
GlXba_bi19@student.itstep.org <GlXba_bi19@student.itstep.org>
2024-09-16 11:09:50 +0300

    Added empty methods

---
+++ arbuzarbuz/vector.cpp
@@ -0,0 +1,82 @@
+#include "Vector.h"
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+
+Vector::Vector()
+{
+	arr = nullptr;
+	size = 0;
+}
+Vector::Vector(int s)
+{
+	size = s;
+	arr = new int[size] {0};
+}
+Vector::~Vector() {
+	delete[]arr;
+	size = 0;
+}
+void Vector::InputRand() {
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 41 - 20;
+	}
+}
+void Vector::Print() {
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << "\t";
+	}
+	cout << "\n---------------------------------------------------\n";
+}
+
+void Vector::PushBack(int a) {
+	int* temp = new int[size + 1];
+
+	for (int i = 0; i < size; i++)
+	{
+		temp[i] = arr[i];
+	}
+	temp[size] = a;
+
+	delete[] arr;
+
+	arr = temp;
+	size++;
+
+}
+
+int Vector::PopBack() {
+	int* temp = new int[size - 1];
+	for (int i = 0; i < size - 1; i++) {
+		temp[i] = arr[i];
+	}
+	int el = arr[size - 1];
+
+	delete[] arr;
+	arr = temp;
+	size--;
+
+	return el;
+}
+
+void Vector::PushForward(int a){
+
+}
+
+int Vector::PopForward(){
+
+	return 0;
+}
+
+void Vector::PushIndex(int a, int index){
+
+}
+
+int Vector::PopIndex(int index){
+
+	return 0;
+}
+
+void Vector::PushArr(int arr[]){
+
+}
