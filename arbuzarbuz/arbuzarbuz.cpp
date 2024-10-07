7c2d1240450b3e546d2df3807bf9399d32364bd3
Cennge <ilyagliba02@gmail.com>
2024-10-07 20:40:56 +0300

    added other figures

--- arbuzarbuz/arbuzarbuz.cpp
+++ arbuzarbuz/arbuzarbuz.cpp
@@ -3,11 +3,16 @@
 #include "Rectangle.h"
 #include "Triangle.h"
 #include "Square.h"
+#include "Trapeze.h"
+#include "Romb.h"
 
 using namespace std;
 
 
 int main() {
-    
+    Romb myRomb;
+    myRomb.SetDimension(10.0, 15.0);
+
+    myRomb.ShowArea();
     return 0;
 }
