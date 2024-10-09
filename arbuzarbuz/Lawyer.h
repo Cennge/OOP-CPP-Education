0028dffee410ae81b42eed303b0dea4501803272
GlXba_bi19@student.itstep.org <GlXba_bi19@student.itstep.org>
2024-10-09 10:53:00 +0300

    added Lawyer

---
+++ arbuzarbuz/Lawyer.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Human.h"
+class Lawyer : public Human
+{
+	char* company;
+	bool finishedUniversity;
+
+public:
+	Lawyer();
+	Lawyer(const char* n, int a, const char* comp, bool finish);
+	~Lawyer();
+	void Output(); 
+	void Input(const char* n, int a, const char* comp, bool finish);
+};
