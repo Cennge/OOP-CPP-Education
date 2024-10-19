4bbe1dde779b25de58f9daca85ccafe7cd7f56a8
Cennge <ilyagliba02@gmail.com>
2024-10-19 18:55:51 +0300

    added military base class

--- arbuzarbuz/arbuzarbuz.cpp
+++ arbuzarbuz/arbuzarbuz.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
+#include "Game.h"
+#include <ctime> 
 
 using namespace std;
 
 int main() {
-  
-    return 0;
+    system("color 3");
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+    Base::people_on_base = rand() % 100;   
+    Base::vehicles_on_base = rand() % 50; 
+    Base::petrol_on_base = rand() % 1000;  
+    Base::goods_on_base = rand() % 200;   
+
+    Game game; 
+    game.showMenu(); 
+
+    return 0; 
 }
