7e775aacfd1dcd351dd347b260b32d2da6783fcf
GlXba_bi19@student.itstep.org <GlXba_bi19@student.itstep.org>
2024-10-28 11:39:33 +0200

    111

--- arbuzarbuz/arbuzarbuz.cpp
+++ arbuzarbuz/arbuzarbuz.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <list>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
 int main() {
-  
+    list<int>ar(10);
+    generate(ar.begin(), ar.end(), []() {return rand() % 41 - 20; });
+
+    ostream_iterator<int> os(cout, "\t");
+    copy(ar.begin(), ar.end(), os);
+
+    int count = count_if(ar.begin(), ar.end(), [](int a) {return a < 0; });
+    cout << count << endl;
+    
+    
+    int minRange, maxRange;
+    cout << "¬ведите диапазон (от и до): ";
+    cin >> minRange >> maxRange;
+
+    copy_if(ar.begin(), ar.end(), [minRange, maxRange](int a) {return return a >= minRange && a <= maxRange; });
+
+
     return 0;
 }
